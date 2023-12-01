04a427a191c180ac0e511e42f4bbe472558fd5f5
Berno20 <ruedabernardo2@gmail.com>
2023-12-01 01:11:33 -0400

    View de repuesto modificada 2

--- view/repuesto.php
+++ view/repuesto.php
@@ -29,17 +29,65 @@ include("header.php");
                                 <p>Asegurate de escribir bien el nombre del repuesto.</p>
                                 <br>  
                                 <form name="formulario" id="formulario" method="POST">
-                                    <input type="text" hidden style="display: none;" name="id_categoria" id="id_categoria" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    <input type="text" hidden style="display: none;" name="id_repuesto" id="id_categoria" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                                     
                                     <div class="input-group mb-3">
                                         
                                         <div class="input-group-prepend">
-                                            <span class="input-group-text" id="inputGroup-sizing-default">Categoría: </span>
+                                            <span class="input-group-text" id="inputGroup-sizing-default">Nombre del repuesto: </span>
                                         </div>
-                                        
-                                        <input type="text" name="nombre_categoria" id="nombre_categoria" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                        <input type="text" name="nombre_repuesto" id="nombre_repuesto" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                                     </div>
-                            </div>
+
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="inputGroup-sizing-default">Codigo: </span>
+                                        </div>
+                                        <input type="text" name="nombre_repuesto" id="nombre_repuesto" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="inputGroup-sizing-default">Categoria: </span>
+                                        </div>
+                                        <input type="text" name="id_categoria" id="id_categoria" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                        <select name="categoria" id="categoria">
+                                        </select>
+                                    </div>
+
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="inputGroup-sizing-default">Medida: </span>
+                                        </div>
+                                        <input type="text" name="medida_repuesto" id="medida_repuesto" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="inputGroup-sizing-default">Stock: </span>
+                                        </div>
+                                        <input type="text" name="stock_repuesto" id="stock_repuesto" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="inputGroup-sizing-default">Stock minimo: </span>
+                                        </div>
+                                        <input type="text" name="stock_minimo" id="stock_minimo" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="inputGroup-sizing-default">Precio sugerido: </span>
+                                        </div>
+                                        <input type="text" name="precio_sugerido" id="precio_sugerido" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+
+                                    <div class="input-group">
+                                        <span class="input-group-text">Descripción</span>
+                                        <textarea class="form-control" aria-label="With textarea" id="descripcion_repuesto" name="descripcion_repuesto"></textarea>
+                                    </div>
+
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-default" data-dismiss="modal" onclick="cancelarform()">Cerrar</button>
                                 <button type="submit" class="btn btn-primary" id="btnGuardar">Guardar</button>
@@ -79,6 +127,5 @@ include("header.php");
 include("footer.php");
 ?>
 <script type="text/javascript" src="script/repuesto.js"></script> 
-<script src="../public/vendors/select2/select2.min.js"></script>
                                 
                                 
